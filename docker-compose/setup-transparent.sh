c1925083ef23e478d3d0e301922e28f55f715a05
Charly Molter <charly.molter@konghq.com>
2021-09-09 11:43:13 +0200

    Add gateway setup for docker-compose setup
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- docker-compose/setup-transparent.sh
+++ docker-compose/setup-transparent.sh
@@ -2,12 +2,13 @@
 
 kumactl install transparent-proxy --skip-resolv-conf --redirect-dns --kuma-dp-user kuma-dp --verbose --redirect-inbound --redirect-dns-upstream-target-chain DOCKER_OUTPUT
 ADDRESS=`ip route list | tail -1 | awk '{ print $7 }'`
+if [[ -z "${DP_FILE}" ]]; then
 cat <<-EOF > /tmp/dp.yaml
 type: Dataplane
 mesh: default
-name: ${SERVICE_NAME}-${HOSTNAME}
+name: {{DP_NAME}}
 networking:
-  address: ${ADDRESS}
+  address: {{ADDRESS}}
   inbound:
     - port: ${PORT}
       servicePort: ${SERVICE_PORT}
@@ -18,5 +19,8 @@ networking:
     redirectPortOutbound: 15001
 
 EOF
+else
+  cp ${DP_FILE} /tmp/dp.yaml
+fi
 cat /tmp/dp.yaml
-sudo -u kuma-dp /usr/bin/kuma-dp run --cp-address=https://${CP_HOSTNAME}:5678 --log-level=debug --dataplane-file=/tmp/dp.yaml
+sudo -u kuma-dp /usr/bin/kuma-dp run --cp-address=https://${CP_HOSTNAME}:5678 --log-level=debug --dataplane-var=ADDRESS=${ADDRESS} --dataplane-var=DP_NAME="${SERVICE_NAME}-${HOSTNAME}" --dataplane-file=/tmp/dp.yaml
