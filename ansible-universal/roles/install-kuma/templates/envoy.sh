859fea608106d30d2281ae32e86d143c41c84e43
James Peach <james.peach@konghq.com>
2021-10-19 09:48:12 +1100

    Install an echo service to the mesh. (#12)
    * Refactor so that the roles you install is for the echo server, and
      that pulls in and configures it's dataplane configuration.
    * Fix the Envoy script to work around container permission problems
      with a different flavor of hackery.
    * Ensure all hosts have updated packages.
    * Update the README to give some pointers to what this is all doing.
    
    Signed-off-by: James Peach <james.peach@konghq.com>

--- ansible-universal/roles/install-kuma/templates/envoy.sh
+++ ansible-universal/roles/install-kuma/templates/envoy.sh
@@ -7,5 +7,21 @@ set -o nounset
 readonly DOCKER=${DOCKER:-podman}
 readonly ENVOY=${ENVOY:-{{envoy.repository}}:{{envoy.version}}}
 
-exec $DOCKER run --network host $ENVOY "$@"
+readonly HERE=$(cd $(dirname $0) && pwd)
+readonly ENVOY_BIN="$HERE/.bin/envoy.{{envoy.version}}"
 
+# This is just atrocious. podman ignores `--user=root` which we need to be able
+# to read the envoy bootstrap, so avoid the whole mess of containerization by
+# copying the envoy binary out of the container. Surprisingly, the binary from
+# the Envoy Ubuntu-based image works fine on Fedora  ¯\_(ツ)_/¯
+if [ ! -x "$ENVOY_BIN" ]; then
+  readonly CONTAINERID="envoy-binary-source-$RANDOM"
+
+  mkdir -p $(dirname "$ENVOY_BIN")
+
+  "$DOCKER" run --detach --security-opt label=disable --name "$CONTAINERID" "$ENVOY" sleep 1d
+  "$DOCKER" cp "$CONTAINERID":/usr/local/bin/envoy "$ENVOY_BIN"
+  "$DOCKER" kill "$CONTAINERID"
+fi
+
+exec "$ENVOY_BIN" "$@"
